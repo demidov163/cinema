9c43d8640619eebdb69710b62dd457196925b2c9
demidov <demidow_1@mail.ru>
2018-05-23 18:37:06 +0400

    tables script has added

---
+++ scripts/all_tables.sql
@@ -0,0 +1,30 @@
+drop table sessions;
+drop table films;
+drop table halls;
+drop type place;
+
+
+
+create table films(
+ id serial primary key,
+ duration integer,
+ name varchar(100),
+ baseprice integer
+);
+
+create type place as (c integer, r integer, vip boolean);
+
+create table halls(
+id serial primary key,
+name varchar(100),
+placecoeff decimal,
+places place[][]
+);
+
+create table sessions(
+id serial primary key,
+film integer references films(id),
+date timestamp,
+hall integer references halls(id),
+price integer check(price > 0)
+);
