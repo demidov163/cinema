193f89641ba21377c6ce79df3e9ba34ce3f11e62
demidov <demidow_1@mail.ru>
2018-05-28 15:50:21 +0400

    session validator added

---
+++ src/main/java/com/demidov/cinema/service/validators/SessionValidator.java
@@ -0,0 +1,24 @@
+package com.demidov.cinema.service.validators;
+
+import com.demidov.cinema.model.entities.Session;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Qualifier("session")
+public class SessionValidator extends AbstractEntityParameterValidator<Session> {
+
+    @Override
+    protected void validateActualEntityParameters(Session entity, StringBuilder errorBuilder) {
+         checkValidation(errorBuilder, validatePrice(entity.getPrice()));
+        //todo add other validation
+    }
+     private Optional<String> validatePrice(Integer price) {
+         if (price == null || price < 0 || price > 500000) {
+             return Optional.of(String.format("Price %d is invalid ", price));
+         }
+         return Optional.empty();
+     }
+}
